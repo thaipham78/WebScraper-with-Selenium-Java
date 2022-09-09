1fa8070f1353d1ab3395d55cefc8591cdfc43b0c
Thai Pham (Steven) <phamthai782000@gmail.com>
2022-09-09 20:53:11 +0700

    Scraper: version 1

---
+++ WebScraper/src/test/java/thaipham/webscraper/AppTest.java
@@ -0,0 +1,114 @@
+package thaipham.webscraper;
+
+import com.google.gson.Gson;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.text.MessageFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+class Product {
+
+    String Product_Name;
+    String Product_Price;
+    String Date;
+    String Source;
+}
+
+public class AppTest {
+
+    ArrayList<String> resultIndexList = new ArrayList<>();
+    ArrayList<WebElement> results = new ArrayList<>();
+    ArrayList<Product> title_price_list = new ArrayList<>();
+    WebDriver browser;
+    WebElement searchAgent;
+
+    public String changePageUrl(String pageNumber) {
+        String url = MessageFormat.format("https://www.amazon.com/s?k=iphone+11&page={0}&crid=19QVE3PB66B8U&qid=1662432049&sprefix=iphone11%2Caps%2C417&ref=sr_pg_{0}", pageNumber);
+        return url;
+    }
+
+    public void generateResultsIndex() {
+        for (int i = 2; i <= 16; i++) {
+            String formatNumber = Integer.toString(i);
+            String indexId = MessageFormat.format("//*[@id=\"search\"]/div[1]/div[1]/div/span[3]/div[2]/div[{0}]", formatNumber);
+            this.resultIndexList.add(indexId);
+        }
+
+    }
+
+    public void generateResults() {
+        for (int i = 0; i < this.resultIndexList.size(); i++) {
+            WebElement result = this.searchAgent.findElement(By.xpath(this.resultIndexList.get(i)));
+            this.results.add(result);
+        }
+
+    }
+
+    public void formatResults() {
+        Product listItem = new Product();
+        String source = "Amazon";
+        LocalDate today = LocalDate.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        String formattedDate = formatter.format(today);
+
+        for (WebElement result : this.results) {
+            try {
+                String name = result.findElement(By.cssSelector(".sg-col-inner .a-section  .sg-row .sg-col-inner h2 a span")).getText();
+                String price = result.findElement(By.cssSelector(".sg-col-inner .a-section  .sg-row .sg-col-inner .sg-row .sg-col-inner a span span:nth-child(2) .a-price-whole")).getText();
+                listItem.Product_Name = name;
+                listItem.Product_Price = price;
+                listItem.Source = source;
+                listItem.Date = formattedDate;
+                this.title_price_list.add(listItem);
+            } catch (Exception e) {
+                System.out.println("An exception occurred");
+            }
+
+        }
+
+    }
+
+    public void generateData(String page) {
+        String url = this.changePageUrl(page);
+        this.browser.get(url);
+        this.searchAgent = this.browser.findElement(By.xpath("//*[@id=\"search\"]/div[1]/div[1]/div/span[3]/div[2]"));
+        this.generateResults();
+        this.formatResults();
+        this.results.clear();
+    }
+
+//    String test = this.changePageUrl("2");
+    @Test
+    public void run() {
+        this.generateResultsIndex();
+        WebDriverManager.chromedriver().setup();
+        this.browser = new ChromeDriver();
+        int counter = 1;
+        while (counter < 4) {
+            String formattedData = Integer.toString(counter);
+            this.generateData(formattedData);
+            counter += 1;
+        }
+        this.browser.quit();
+        int finalData = this.title_price_list.size();
+        String path = "../data.json";
+        String json = new Gson().toJson(this.title_price_list);
+        try ( PrintWriter out = new PrintWriter(new FileWriter(path))) {
+            out.write(json);
+        } catch (Exception e) {
+            System.out.print("Something Wrong !");
+        }
+        System.out.print(json);
+        System.out.println("Total rows: " + finalData);
+
+    }
+
+}
